ae933eb52c7f57259d06e356ff245a4cf11dc401
jcancode <j.gonzalez4876@gmail.com>
2023-01-03 02:00:05 -0500

    Edited CSS styling of HTML file

--- src/index.js
+++ src/index.js
@@ -45,24 +45,3 @@ function showTemperature(response) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
-
-function searchLocation(position) {
-  let latitude = position.coords.latitude;
-  console.log(latitude);
-  let longitude = position.coords.latitude;
-  console.log(longitude);
-  alert(
-    `Your current location coordinates are ${latitude} latitude and ${longitude} longitude`
-  );
-  let apiKey = "8161b4309ee03faae957729ba7104797";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}&apiid=${apiKey}`).then(showTemperature);
-}
-
-function showPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let cityLocation = document.querySelector("#location-button");
-cityLocation.addEventListener("click", showPosition);
